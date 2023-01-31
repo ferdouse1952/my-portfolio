2b066719da7d16170d71d4fcc8d880f211d29645
Ferdouse Binte Asad <ferdousebinteasad@gmail.com>
2023-01-31 18:09:02 +0600

    add

--- src/components/ReactCard/ReactCard.js
+++ src/components/ReactCard/ReactCard.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./ReactCard.css";
 
 const ReactCard = (props) => {
-  const { postImage } = props.projects;
+  const { postImage, title, desc, button } = props.projects;
   return (
     <div>
       <div className="col">
-        <div className="card h-100">
+        <div className="card border-0 h-100">
           <div className="projetsImg">
             <img src={postImage} className="card-img-top" alt="..." />
+            <div className="project-details text-center">
+              <h5>{title}</h5>
+              <p>{desc}</p>
+              <a href={button}><button className="border-0 liveBtn p-2">See Live</button></a>
+            </div>
           </div>
         </div>
       </div>
